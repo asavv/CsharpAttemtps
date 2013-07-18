c7c1dfa598892349924329feea53d3fe35a10861
Ana Vila Verde <ana@symplectic.co.uk>
2013-07-18 16:10:26 +0100

    Solved Chapter 7 exercises.

--- codeSamples/Chapter 7/Classes/Classes/Point.cs
+++ codeSamples/Chapter 7/Classes/Classes/Point.cs
@@ -8,8 +8,46 @@ using System.Text;
 
 namespace Classes
 {
+        // used to represent the location of a point defined by a pair of x and y coordinates.
     class Point
     {
-        // to do
+        private int x, y;
+        private static int objCount = 0;
+
+        // default contructor
+        public Point()
+        {
+            this.x = -1;
+            this.y = -1;
+
+            objCount++;
+
+            //what was initially in this constructor
+            Console.WriteLine("Default contructor called.");
+        }
+        // constructor that prints coordinates on console
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+
+            objCount++;
+
+            //what was initially in this constructor
+            Console.WriteLine("x:{0}, y:{1}", x, y);
+        }
+
+        public double DistanceTo(Point other)
+        {
+            int xDiff = this.x - other.x;
+            int yDiff = this.y - other.y;
+
+            return (Math.Sqrt(Math.Pow(xDiff, 2) + Math.Pow(yDiff, 2)));
+        }
+
+        public static int ObjectCount()
+        {
+            return objCount;
+        }
     }
 }
--- codeSamples/Chapter 7/Classes/Classes/Program.cs
+++ codeSamples/Chapter 7/Classes/Classes/Program.cs
@@ -10,9 +10,21 @@ namespace Classes
 {
     class Program
     {
+        
         static void DoWork()
         {
-            // to do
+            Point origin = new Point();
+            Point bottomRight = new Point(1024, 1280);
+
+            // calling the method DistanceTo with object origin, sets 'this' to x=y=-1 . So 'this' refers to the object that called teh function.
+            // and you use as arguments bottomRight, with sets 'other' as x=1028 and y=1280.
+            double distance = origin.DistanceTo(bottomRight);  
+
+
+
+            Console.WriteLine("Distance is: {0}", distance);
+
+            Console.WriteLine("Number of point objects: {0}",Point.ObjectCount());
         }
 
         static void Main(string[] args)

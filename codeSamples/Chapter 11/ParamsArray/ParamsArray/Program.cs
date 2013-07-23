fab34b5d9c18bb7e01a15bf1d10fc93cd4a280fa
Ana Vila Verde <ana@symplectic.co.uk>
2013-07-23 17:38:27 +0100

    Solved chapter 11 exercises

--- codeSamples/Chapter 11/ParamsArray/ParamsArray/Program.cs
+++ codeSamples/Chapter 11/ParamsArray/ParamsArray/Program.cs
@@ -12,7 +12,17 @@ namespace ParamsArray
     {
         static void DoWork()
         {
-            // to do		
+            // // testing the exceptions are working...
+            //Console.WriteLine(Util.Sum(null));
+            //Console.WriteLine(Util.Sum());	
+
+            Console.WriteLine(Util.Sum(10,9,8,7,6,5,4,3,2,1));	
+
+            Console.WriteLine(Util.Sum(2,4,6,8));
+
+            Console.WriteLine(Util.Sum(2, 4, 6));
+
+            Console.WriteLine(Util.Sum(2, 4, 6, 8, 10));
         }
 
         static void Main()
@@ -24,6 +34,7 @@ namespace ParamsArray
             catch (Exception ex)
             {
                 Console.WriteLine("Exception: {0}", ex.Message);
+                Console.WriteLine("Located in: {0}", ex.StackTrace);
             }
         }
     }
--- codeSamples/Chapter 11/ParamsArray/ParamsArray/Util.cs
+++ codeSamples/Chapter 11/ParamsArray/ParamsArray/Util.cs
@@ -6,6 +6,40 @@ namespace ParamsArray
 {
 	class Util
 	{
-		// to do
+        /// <summary>
+        /// Returns the sum of all the values given in parameter list.
+        /// </summary>
+        /// <param name="paramList">Integer parameter list.</param>
+        /// <exception cref="ArgumentException">Thrown when parameter list is either empty or null.</exception>
+        ///  <returns></returns>
+		public static int Sum(params int[] paramList)
+		{
+            Console.WriteLine("Using parameter list.");
+		    if (paramList == null)
+		    {
+		        throw new ArgumentException("Util.Sum: null parameter list");
+		    }
+
+            if (paramList.Length == 0)
+            {
+                throw new ArgumentException("Util.Sum: empty parameter list");
+            }
+
+		    int sumTotal = 0;
+
+		    foreach (int i in paramList)
+		    {
+		        sumTotal += i;
+		    }
+
+		    return sumTotal;
+		}
+
+        public static int Sum(int param1 = 0, int param2 = 0, int param3 = 0, int param4 = 0)
+        {
+            Console.WriteLine("Using optional parameters.");
+            int sumTotal = param1 + param2 + param3 + param4;
+            return sumTotal;
+        }
 	}
 }

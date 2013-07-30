ddfe8f4bad9476809faf17e9602c93517be860a0
Ana Vila Verde <ana@symplectic.co.uk>
2013-07-30 11:25:45 +0100

    Added John's examples on use of function pointers

---
+++ FunctionPointers_byJohn/FunctionPointers_byJohn/FunctionPointers_byJohn/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionPointers_byJohn
+{
+    public delegate void BuilderProgressCallback(int stage);
+
+    public class House
+    {
+        private int mStageReached;
+
+        public int StageReached
+        {
+            get { return mStageReached; }
+            set { mStageReached = value; }
+        }
+    }
+
+    public class HouseBuilder
+    {
+        private readonly BuilderProgressCallback mCallback;
+
+        public HouseBuilder(BuilderProgressCallback callback)
+        {
+            mCallback = callback;
+        }
+
+        /// <summary>
+        /// I promise to call your number every time I complete a stage of work, should you have given me a number.
+        /// </summary>
+        /// <returns></returns>
+        public House BuildHouse()
+        {
+            House house = new House();
+            house.StageReached = 1;
+            if (mCallback != null) mCallback(1);
+            house.StageReached = 2;
+            if (mCallback != null) mCallback(2);
+            house.StageReached = 3;
+            if (mCallback != null) mCallback(3);
+            house.StageReached = 4;
+            if (mCallback != null) mCallback(4);
+            house.StageReached = 5;
+            if (mCallback != null) mCallback(5);
+            return house;
+        }
+    }
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            HouseBuilder bob = new HouseBuilder(Number999);
+            HouseBuilder amy = new HouseBuilder(Number07811105343);
+            House houseBob = bob.BuildHouse();
+            House houseAmy = amy.BuildHouse();
+
+        }
+
+        private static void Number07811105343(int stage)
+        {
+            Console.WriteLine("Stage {0} complete.", stage);
+        }
+
+        private static void Number999(int stage)
+        {
+            Console.WriteLine("You are wasting police time!", stage);
+        }
+    }
+}
---
+++ FunctionPointers_byJohn/FunctionPointers_byJohn_2/FunctionPointers_byJohn_2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionPointers_byJohn_2
+{
+    public delegate void FunctionPointer(int value);
+
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Human bob = new Human("Bob");
+            Human sarah = new Human("Sarah");
+
+            FunctionPointer pointers = null;
+            pointers += bob.CallMyPhone;
+            bob = null;
+            pointers += Program.Number999;
+            pointers += sarah.CallMyPhone;
+            pointers += Program.Number07811105343;
+
+
+            pointers(4); // executed the functions pointed by "pointers" function pointer.
+        }
+
+        private static void Number07811105343(int stage)
+        {
+            Console.WriteLine("Stage {0} complete.", stage);
+        }
+
+        private static void Number999(int stage)
+        {
+            Console.WriteLine("You are wasting police time!", stage);
+        }
+    }
+
+    public class Human
+    {
+        public string Name;
+
+        public Human(string name)
+        {
+            Name = name;
+        }
+
+        public void CallMyPhone(int stage)
+        {
+            Console.WriteLine("Hello. {0} speaking! I hear you are at stage {1}", Name, stage);
+        }
+    }
+}

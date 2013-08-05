550cb4d104bae25af22a45c173a678b37c8ea2f9
Ana Vila Verde <ana@symplectic.co.uk>
2013-08-05 13:58:25 +0100

    Small push so I can check that my JohnExercise data isn't lost...

---
+++ codeSamples/Chapter 18/BinaryTree/BinaryTree/Tree.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class Tree<TItem> where TItem : IComparable<TItem>
+    {
+        public TItem NodeData { get; set; }
+        public Tree<TItem> LeftTree { get; set; }
+        public Tree<TItem> RightTree { get; set; } 
+
+        public Tree(TItem nodeValue)
+        {
+            this.NodeData = nodeValue;
+            this.LeftTree = null;
+            this.RightTree = null;
+        }
+
+        /// <summary>
+        /// Insert a new item in the tree. Method assumes the tree is not empty.
+        /// </summary>
+        /// <param name="newItem"></param>
+        public void Insert(TItem newItem)
+        {
+            TItem currentNodeValue = this.NodeData;
+
+            if (currentNodeValue.CompareTo(newItem) > 0)
+            {
+                //Insert new item into the left substree
+                if (this.LeftTree == null)
+                {
+                    this.LeftTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    this.LeftTree.Insert(newItem);
+                }
+            }
+            else
+            {
+                //Insert new item into the right subtree
+                if (this.RightTree == null)
+                {
+                    this.RightTree = new Tree<TItem>(newItem);
+                }
+                else
+                {
+                    this.RightTree.Insert(newItem);
+                }
+            }
+        }
+    
+    }
+}
